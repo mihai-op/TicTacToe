04404cee945953b8ef1b4dd8f1b2467b378bd0f0
Mihai Oprea <opreamihaialex@gmail.com>
2017-12-20 13:13:47 +0200

    Players do not belong anymore to separated properties of Game class. Fixes #13

--- src/Game.php
+++ src/Game.php
@@ -4,24 +4,21 @@ namespace TicTacToe;
 
 class Game {
     private $board;
-    private $firstPlayer;
-    private $secondPlayer;
-
-    private $playerAtTurn;
+    private $players;
 
     public function __construct($firstPlayer, $secondPlayer, $symbols = ['X','O']) {
         $this->board = new Board;
 
-        $this->firstPlayer = $firstPlayer;
-        $this->secondPlayer = $secondPlayer;
+        $this->players[0] = $firstPlayer;
+        $this->players[1] = $secondPlayer;
 
-        $this->firstPlayer->setGame($this);
-        $this->secondPlayer->setGame($this);
+        $this->players[0]->setGame($this);
+        $this->players[1]->setGame($this);
 
-        $this->firstPlayer->setSymbol($symbols[0]);
-        $this->secondPlayer->setSymbol($symbols[1]);
+        $this->players[0]->setSymbol($symbols[0]);
+        $this->players[1]->setSymbol($symbols[1]);
 
-        $this->playerAtTurn = $firstPlayer;
+        $this->playerAtTurn = $this->players[0];
     }
 
 
@@ -36,11 +33,11 @@ class Game {
             $this->playerAtTurn->markTile($tile, $currentPlayer->getSymbol());
 
             //I need to find a better way to compare objects
-            if($this->playerAtTurn->getSymbol() === $this->firstPlayer->getSymbol()) {
-                $this->playerAtTurn = $this->secondPlayer;
+            if($this->playerAtTurn->getSymbol() === $this->players[0]->getSymbol()) {
+                $this->playerAtTurn = $this->players[1];
             }
             else {
-                $this->playerAtTurn = $this->firstPlayer;
+                $this->playerAtTurn = $this->players[0];
             }
         }
     }
@@ -60,11 +57,11 @@ class Game {
         $lineStats[] = $this->sameSymbolOnLine($this->board->getSecondaryDiagonal());
 
         for($index = 0; $index < count($lineStats); $index++) {
-            if($lineStats[$index] === $this->firstPlayer->getSymbol()) {
-                return $this->firstPlayer;
+            if($lineStats[$index] === $this->players[0]->getSymbol()) {
+                return $this->players[0];
             }
-            else if($lineStats[$index] === $this->secondPlayer->getSymbol()) {
-                return $this->secondPlayer;
+            else if($lineStats[$index] === $this->players[1]->getSymbol()) {
+                return $this->players[1];
             }
         }
 
